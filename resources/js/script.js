798145945ecb18fd362805972f37aa9498a33c79
DavideGV <dav.vernia@gmail.com>
2023-08-28 17:41:58 +0200

    aggiunta animazione searchbar

---
+++ resources/js/script.js
@@ -0,0 +1,8 @@
+let navSearch = document.querySelector('#navSearch');
+let searchButton = document.querySelector('#searchButton');
+
+searchButton.addEventListener('mouseenter', ()=> {
+    navSearch.style.opacity = '1';
+    navSearch.style.width = '230px';
+
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,13 @@
     <div class="container my-5">
         <div class="row">
             @forelse($articles as $article)
-                <div class="col-3 col-md-3 zoom">
+                <div class="col-12 col-md-6 col-lg-4 zoom">
 
                     @if ($article->category)
                         <div class="col-3 zoom">
 
                         </div>
-                        {{-- <a href="{{ route('article.byCategory', ['category' => $article->category->id]) }}"
-                            class="small text-muted fst-italic text-capitalize">{{ $article->category->name }}</a> --}}
+
                     @else
                         <p class="small text-muted fst-italic text-capitalize">
                             Non categorizzato
@@ -21,7 +20,7 @@
 
                     <div class="card">
                         <div class="card-body">
-                        <img src="{{ Storage::url($article->image) }}" alt="Immagine dell'articolo" class="col-md-12 rounded-3">
+                        <img src="{{ Storage::url($article->image) }}" alt="Immagine dell'articolo" class="img-fluid">
                         {{-- <img src="https://picsum.photos/250/170?random={{$article->id}}" alt="Immagine di repertorio"> --}}
                             <hr>
                             <h3 class="card-title">{{ $article->title }}</h3>
@@ -45,12 +44,14 @@
                     </div>
                 </div>
 
-            @empty
-                @if (Auth::check() && Auth::user()->is_writer)
-                    <h5 class="text-center pt-5">Non ci sono ancora articoli, <a href="{{ route('article.create') }}"
-                            class="btn custom-1">Inseriscine uno</a></h5>
-                @endif
-            @endforelse
+                @empty
+                    @if (Auth::check() && Auth::user()->is_writer)
+                        <h5 class="text-center pt-5">Non ci sono ancora articoli, <a href="{{ route('article.create') }}"
+                                class="btn custom-1">Inseriscine uno</a></h5>
+                    @endif
+
+                @endforelse
+                
 
         </div>
 
