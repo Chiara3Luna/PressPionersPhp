9bf77cc42de0149e5072d930956e52c6314589ea
DavideGV <dav.vernia@gmail.com>
2023-08-28 17:44:26 +0200

    modifiche

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,5 +4,4 @@ let searchButton = document.querySelector('#searchButton');
 searchButton.addEventListener('mouseenter', ()=> {
     navSearch.style.opacity = '1';
     navSearch.style.width = '230px';
-
 });
