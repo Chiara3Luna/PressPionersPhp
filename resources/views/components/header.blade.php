7d7570f0d529f24ba5a39c50e7193278d11f3294
DavideGV <dav.vernia@gmail.com>
2023-08-28 14:41:10 +0200

    corretta sezione chi siamo

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,6 +2,5 @@
 
     <h1 class="text-center pb-2 display-1 mt-4">“The Aulab Post„</h1>
     <h2 class="text-center pb-2 display-6 fw-bold">SEZIONE NOTIZIE</h2>
-    <div class="row justify-content-center">
 
 </div>
